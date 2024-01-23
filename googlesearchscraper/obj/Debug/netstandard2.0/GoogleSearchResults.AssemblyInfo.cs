684fe30c80b1c25e975014dc9bd475dc8656c850
maxmanusv1 <60714489+maxmanusv1@users.noreply.github.com>
2024-01-23 18:40:54 +0300

    rebuilded as netstandard2.0

--- googlesearchscraper/obj/Debug/netstandard2.0/GoogleSearchResults.AssemblyInfo.cs
+++ googlesearchscraper/obj/Debug/netstandard2.0/GoogleSearchResults.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Library to get Google Search results and grab some backlinks for related keywords" +
     ". No Google API key needed, better use proxy after a little usage.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoogleSearchResults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoogleSearchResults")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.2.1.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
